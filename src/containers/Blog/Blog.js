9b501cfe01e193336b7262a0b872e364fa62a882
tonaramos <gtonaramos@gmail.com>
2018-10-15 23:28:40 -0700

    Added link to function of react-routern and formated spacing in files.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -39,16 +40,13 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        {/* <Posts /> */}
+        {/* <Postcs   /> */}
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-        {/* <section>
-            <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -21,12 +22,12 @@ class Posts extends Component {
             author: 'Tona'
           }
         });
-        this.setState({posts: updatedPosts});
+        this.setState({ posts: updatedPosts });
         // console.log( response );
       })
       .catch(error => {
         // console.log(error);
-        // this.setState({error: true});
+        this.setState({error: true});
       });
   }
 
@@ -38,12 +39,14 @@ class Posts extends Component {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post 
-          key={post.id} 
-          title={post.title} 
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)} 
-        />;
+        return (
+        <Link to={'/' + post.id} key={post.id}>
+          <Post
+            title={post.title} 
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)} 
+          />
+        </Link> );
       });
     }
     return(
